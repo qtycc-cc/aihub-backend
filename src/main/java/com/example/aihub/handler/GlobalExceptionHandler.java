9be10f8303a12eddf55d648d5b963c9407858e48
Carmen <2942173241@qq.com>
2025-03-07 13:52:35 +0800

    添加异常返回对象

--- src/main/java/com/example/aihub/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/aihub/handler/GlobalExceptionHandler.java
@@ -1,7 +1,5 @@
 package com.example.aihub.handler;
 
-import java.util.Map;
-
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,32 +7,38 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.example.aihub.exception.InvalidCredentialsException;
-import com.example.aihub.utils.JsonUtils;
+import com.example.aihub.pojo.ExceptionResponse;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(DuplicateKeyException.class)
     @ResponseBody
-    public ResponseEntity<?> handleDuplicateKeyException(DuplicateKeyException e) {
-        return ResponseEntity.internalServerError().body(JsonUtils.toJson(
-            Map.of("reason", e.getMessage())
-        ));
+    public ResponseEntity<ExceptionResponse> handleDuplicateKeyException(DuplicateKeyException e) {
+        return ResponseEntity.internalServerError().body(
+            ExceptionResponse.builder()
+                            .reason(e.getMessage())
+                            .build()
+        );
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseBody
-    public ResponseEntity<?> handleIllegalArgumentException(IllegalArgumentException e) {
-        return ResponseEntity.badRequest().body(JsonUtils.toJson(
-            Map.of("reason", e.getMessage())
-        ));
+    public ResponseEntity<ExceptionResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.badRequest().body(
+            ExceptionResponse.builder()
+                            .reason(e.getMessage())
+                            .build()
+        );
     }
 
     @ExceptionHandler(InvalidCredentialsException.class)
     @ResponseBody
-    public ResponseEntity<?> handleInvalidCredentialsException(InvalidCredentialsException e) {
-        return ResponseEntity.badRequest().body(JsonUtils.toJson(
-            Map.of("reason", e.getMessage())
-        ));
+    public ResponseEntity<ExceptionResponse> handleInvalidCredentialsException(InvalidCredentialsException e) {
+        return ResponseEntity.badRequest().body(
+            ExceptionResponse.builder()
+                            .reason(e.getMessage())
+                            .build()
+        );
     }
 }
